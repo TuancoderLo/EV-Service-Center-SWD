4a8864bbec588b55591a90bc9d6bcfcfb123dc4e
TuancoderLo <tuantmse180360@fpt.edu.vn>
2025-10-07 21:53:56 +0700

    update lại logic file NotFound404

--- src/app/NotFound404/page.tsx
+++
@@ -1,5 +0,0 @@
-import NotFoundPage from "@/components/pages/NotFoundPage";
-
-export default function NotFound404Page() {
-  return <NotFoundPage />;
-}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import NotFoundPage from "@/components/pages/NotFoundPage";
 
 export default function NotFound() {
-  redirect("/NotFound404");
+  return <NotFoundPage />;
 }
